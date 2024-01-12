883360b37cb07bc008e40b070f6f30c948e77e7c
NitinKushwaha <nitinkushwaha706@gmail.com>
2024-01-12 11:40:37 +0530

    twitter icon changes to telgram

--- src/SideBar/Social/SocialMedia.jsx
+++ src/SideBar/Social/SocialMedia.jsx
@@ -6,7 +6,7 @@ export default function SocialMedia () {
             <a href="https://www.linkedin.com/in/nitinkushwaha19/"><i className="fa-brands fa-linkedin"></i></a>
             <a href="https://github.com/Nitinkushwaha19"><i className="fa-brands fa-github"></i></a>
             <a href="https://www.facebook.com/nitin.kushwaha.7737"><i className="fa-brands fa-square-facebook"></i></a>
-            <a href="#"><i className="fa-brands fa-square-twitter"></i></a>
+            <a href="https://t.me/Nitinkushwa"><i class="fa-brands fa-telegram"></i></a>
             <a href="https://www.instagram.com/nitin_kushwaha19/"><i className="fa-brands fa-instagram"></i></a>
         </div>
     );
