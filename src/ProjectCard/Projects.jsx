09416da45da1dce2972663d6418da0369a0d14b9
NitinKushwaha19 <nitinkushwaha706@gmail.com>
2025-07-25 19:24:17 +0530

    Added prepwise project

--- src/ProjectCard/Projects.jsx
+++ src/ProjectCard/Projects.jsx
@@ -4,12 +4,23 @@ import portfolio from '../assets/portfolio.png'
 import newsapp from '../assets/newsapp.png'
 import HavenHub from '../assets/HavenHub.png'
 import chatApp from "../assets/chatApp.png";
+import prepwise from "../assets/image.png"
 
 export default function Projects() {
   return (
     <div className="project-collection">
       <h5>My Projects</h5>
       <div className="project-items">
+
+         <ProjectCard
+          Title="AI-Powered Interview Practice Platform"
+          Description="AI interview web app with voice input, resume upload, and personalized question generation using Vapi AI, Gemini AI, and a responsive UI with shadcn/ui."
+          useTech="Next.js, TypeScript, Firebase, Vapi Ai, Gemini"
+          gitHubLink="https://github.com/Nitinkushwaha19/PrepWise"
+          projectLink="https://my-portfolio-git-main-nitin-kushwahas-projects.vercel.app/"
+          projectImage={prepwise}
+        />
+        
         <ProjectCard
           Title="HavenHub"
           Description="HavenHub is a full-stack rental PG platform where users can explore PG options, check prices, and connect with owners."
@@ -37,7 +48,7 @@ export default function Projects() {
           Description="Discover my responsive portfolio website, crafted with React.js, HTML, CSS, JavaScript, and MUI icons, showcasing my work in a dynamic, user-friendly interface."
           useTech="React js, Nodemailer, MUI, Tailwind css"
           gitHubLink="https://github.com/Nitinkushwaha19/My-portfolio"
-          projectLink="https://my-portfolio-git-main-nitin-kushwahas-projects.vercel.app/"
+          projectLink="https://prep-wise-coral-psi.vercel.app/home/"
           projectImage={portfolio}
         />
         
